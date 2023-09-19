fd4bd7c5311560efc7b0fe7f9a280ee550efd435
Matheus Silveira <matheuspmsilveira@gmail.com>
2023-09-19 19:19:05 -0300

    feat: implemente lock in terraform state

---
+++ dynamodb.tf
@@ -0,0 +1,16 @@
+// Criando um DynamoDB para armazenar o lock do state do terraform
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+}
